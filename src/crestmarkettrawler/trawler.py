0483f44f1ff9c1cb130c4c4a520fc7c25adad96d
James Muscat <jamesremuscat@gmail.com>
2016-03-09 00:22:08 +0000

    Objectify.

--- src/crestmarkettrawler/trawler.py
+++ src/crestmarkettrawler/trawler.py
@@ -10,20 +10,23 @@ WORMHOLE_REGIONS_START = 11000000
 REQUESTS_PER_SECOND = 50
 
 
-def trawlMarket():
-    Session.get = RateLimited(REQUESTS_PER_SECOND)(Session.get)
-    eve = pycrest.EVE(cache_dir='cache/')
-    items = [34, 35, 36]
-    regions = [region() for region in eve().regions().items if region.id < WORMHOLE_REGIONS_START or region.id == THERA_REGION]
-    while True:
-        region = choice(regions)
-        logging.info("Trawling region {0}".format(region.name))
-        for item in items:
-            sellOrders = region.marketSellOrders(type="https://public-crest.eveonline.com/types/{0}/".format(item)).items
-            buyOrders = region.marketBuyOrders(type="https://public-crest.eveonline.com/types/{0}/".format(item)).items
-            orders = sellOrders + buyOrders
-            logging.info("Retrieved {0} orders for type {1}".format(len(orders), item))
+class Trawler(object):
+    def __init__(self):
+        Session.get = RateLimited(REQUESTS_PER_SECOND)(Session.get)
+        self._eve = pycrest.EVE(cache_dir='cache/')
+
+    def trawlMarket(self):
+        items = [34, 35, 36]
+        regions = [region() for region in self._eve().regions().items if region.id < WORMHOLE_REGIONS_START or region.id == THERA_REGION]
+        while True:
+            region = choice(regions)
+            logging.info("Trawling region {0}".format(region.name))
+            for item in items:
+                sellOrders = region.marketSellOrders(type="https://public-crest.eveonline.com/types/{0}/".format(item)).items
+                buyOrders = region.marketBuyOrders(type="https://public-crest.eveonline.com/types/{0}/".format(item)).items
+                orders = sellOrders + buyOrders
+                logging.info("Retrieved {0} orders for type {1}".format(len(orders), item))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    trawlMarket()
+    Trawler().trawlMarket()
