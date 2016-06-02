9a033cb68f52ffd11420045e26c1b7478a0be716
James Muscat <james.muscat@alces-software.com>
2016-06-02 17:40:07 +0100

    Argh deduping

--- src/crestmarkettrawler/postgres.py
+++ src/crestmarkettrawler/postgres.py
@@ -3,7 +3,6 @@ import gevent
 from gevent import monkey
 gevent.monkey.patch_all()  # nopep8
 
-from psycopg2._psycopg import IntegrityError
 from Queue import Queue
 from threading import Thread
 import datetime
@@ -31,7 +30,7 @@ def rangeAdapter(rangeStr):
 class PostgresAdapter(Thread):
     def __init__(self, statsCollector):
         Thread.__init__(self)
-        self._queue = Queue(maxsize=10)
+        self._queue = Queue(maxsize=1)
         self.setDaemon(True)
 
         self.statsCollector = statsCollector
@@ -49,55 +48,73 @@ class PostgresAdapter(Thread):
     def run(self):
         conn = psycopg2.connect(user = os.environ.get("POSTGRES_USERNAME"), password = os.environ.get("POSTGRES_PASSWORD"), database = os.environ.get("POSTGRES_DB"), host = os.environ.get("POSTGRES_HOST", "localhost"))
 
+        with conn.cursor() as cursor:
+            cursor.execute("PREPARE insert_order (bigint, bigint, bigint, numeric, integer, smallint, integer, integer, boolean, timestamp without time zone, smallint, bigint, bigint, timestamp without time zone) AS INSERT INTO live_orders(orderID, typeID, regionID, price, volRemaining, range, volEntered, minVolume, isBid, issueDate, duration, stationID, solarSystemID, expiry) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)")
+            conn.commit()
+
         duplicateOrderID = re.compile('Key \(orderid\)=\(([0-9]+)\) already exists')
+
+        def deleteDupe(dup):
+            with conn.cursor() as cursor:
+                logger.warn("Manually deleting order with duplicate ID {}".format(dup))
+                cursor.execute("DELETE FROM live_orders WHERE orderid=%s", [dup])
+                conn.commit()
+
+        def processRegion(regionID, orders):
+            try:
+                with conn.cursor() as cursor:
+
+                    logger.info("Processing {} orders for region {}".format(len(orders), regionID))
+
+                    # The time it takes to trawl CREST pages means that orders move between pages between calls, so we
+                    # might encounter the same order ID more than once. When this happens it's safe(ish) to ignore it.
+                    seen = set()
+                    uniqueOrders = [seen.add(o.id) or o for o in orders if o.id not in seen]
+                    logger.info("{} of {} orders have unique IDs".format(len(uniqueOrders), len(orders)))
+
+                    cursor.execute("DELETE FROM live_orders WHERE regionID=%s", [regionID])
+                    for order in uniqueOrders:
+                        try:
+                            realIssueDate = dateutil.parser.parse(order.issued)
+                            expiry = realIssueDate + datetime.timedelta(days = order.duration)
+                            cursor.execute(
+                                "EXECUTE insert_order (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                                (
+                                    order.id,
+                                    order.type,
+                                    regionID,
+                                    order.price,
+                                    order.volume,
+                                    rangeAdapter(order.range),
+                                    order.volumeEntered,
+                                    order.minVolume,
+                                    order.buy,
+                                    order.issued,
+                                    order.duration,
+                                    order.stationID,
+                                    None,
+                                    expiry
+                                )
+                            )
+                            self.statsCollector.tally("database_submitted")
+                            self.statsCollector.datapoint("database_last_updated", datetime.datetime.now().isoformat())
+                        except psycopg2.IntegrityError as e:
+                            conn.rollback()
+                            m = duplicateOrderID.search(e.message)
+                            if m:
+                                logger.warn("Found duplicate order ID {}".format(order.id))
+                                deleteDupe(order.id)
+                                raise e
+
+            except Exception:
+                logger.warn("Failed to process region {}, retrying.".format(regionID))
+                processRegion(regionID, orders)
+            else:
+                conn.commit()
+            logger.info("Finished processing region {}".format(regionID))
 
         while True:
             (regionID, orders) = self._queue.get()
+            self.statsCollector.datapoint("postgres_queue_size", self._queue.qsize())
+            processRegion(regionID, orders)
 
-            cursor = conn.cursor()
-
-            logger.info("Processing {} orders for region {}".format(len(orders), regionID))
-
-            cursor.execute("PREPARE insert_order (bigint, bigint, bigint, numeric, integer, smallint, integer, integer, boolean, timestamp without time zone, smallint, bigint, bigint, timestamp without time zone) AS INSERT INTO live_orders(orderID, typeID, regionID, price, volRemaining, range, volEntered, minVolume, isBid, issueDate, duration, stationID, solarSystemID, expiry) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)")
-
-
-            cursor.execute("DELETE FROM live_orders WHERE regionID=%s", [regionID])
-
-            for order in orders:
-                realIssueDate = dateutil.parser.parse(order.issued)
-                expiry = realIssueDate + datetime.timedelta(days = order.duration)
-                try:
-                    cursor.execute(
-                        "EXECUTE insert_order (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                        (
-                            order.id,
-                            order.type,
-                            regionID,
-                            order.price,
-                            order.volume,
-                            rangeAdapter(order.range),
-                            order.volumeEntered,
-                            order.minVolume,
-                            order.buy,
-                            order.issued,
-                            order.duration,
-                            order.stationID,
-                            None,
-                            expiry
-                        )
-                    )
-                    self.statsCollector.tally("database_submitted")
-                    self.statsCollector.datapoint("database_last_updated", datetime.datetime.now().isoformat())
-                except IntegrityError as e:
-                    logger.error(e.message)
-                    m = duplicateOrderID.search(e.message)
-                    if m:
-                        conn.rollback()
-                        cursor.execute("DELETE FROM live_orders WHERE orderID=%s", [order.id])
-                        self._queue.put(regionID, orders)  # If at first you don't succeed...
-
-            logger.info("Finished processing region {}".format(regionID))
-
-            cursor.execute("DEALLOCATE insert_order")
-            conn.commit()
-            cursor.close()
