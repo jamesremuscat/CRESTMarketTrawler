992ec54e39df419ce944f9d48ca8cc795dd6e870
James Muscat <jamesremuscat@gmail.com>
2016-09-26 18:25:56 +0100

    Don't leave the database connection open when we don't need it.
    We'll be calling this endpoint infrequently enough that it doesn't make sense
    to leave the connection open; and we don't need to worry about recovering from
    errors within a transaction if they should happen.

--- web/app/main.py
+++ web/app/main.py
@@ -8,19 +8,17 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
-conn = psycopg2.connect(
-    user=os.environ.get("POSTGRES_USERNAME"),
-    password=os.environ.get("POSTGRES_PASSWORD"),
-    database=os.environ.get("POSTGRES_DB"),
-    host=os.environ.get("POSTGRES_HOST", "localhost")
-)
-
-
 @app.route("/prices")
 def prices():
-    with conn.cursor() as cur:
-        cur.execute("SELECT typeid, buy_price, buy_volume, buy_min, buy_max, buy_sd, sell_price, sell_volume, sell_min, sell_max, sell_sd, median_price, time FROM live_prices")
-        return json.jsonify(cur.fetchall())
+    with psycopg2.connect(
+        user=os.environ.get("POSTGRES_USERNAME"),
+        password=os.environ.get("POSTGRES_PASSWORD"),
+        database=os.environ.get("POSTGRES_DB"),
+        host=os.environ.get("POSTGRES_HOST", "localhost")
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT typeid, buy_price, buy_volume, buy_min, buy_max, buy_sd, sell_price, sell_volume, sell_min, sell_max, sell_sd, median_price, time FROM live_prices")
+            return json.jsonify(cur.fetchall())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
